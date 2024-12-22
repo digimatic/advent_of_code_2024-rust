e0fd4782858494eedb2ad333a0f652d7a387c444
Peter Westerström <peter@westerstrom.com>
2024-12-22 23:30:49 +0100

    Day 21 solutions in rust

--- src/bin/day21.rs
+++ src/bin/day21.rs
@@ -1,6 +1,12 @@
 use advent_of_code_2024_rust::vec2i::Vec2i;
+use itertools::Itertools;
 use maplit::hashmap;
-use std::{collections::{HashMap, HashSet}, env, fs};
+use std::{
+    cell::RefCell,
+    collections::{HashMap, VecDeque},
+    env, fs,
+    rc::Rc,
+};
 
 fn parse(input: &str) -> Vec<String> {
     input.lines().map(|line| line.to_string()).collect()
@@ -36,188 +42,166 @@ fn get_dirpad() -> HashMap<char, Vec2i> {
     pad
 }
 
-fn all_variants(moves: Vec<Vec<Moves>>) -> Vec<Moves> {
-    fn combine(current: &mut Moves, parts: &[Vec<Moves>], result: &mut Vec<Moves>) {
-        if parts.is_empty() {
-            result.push(current.clone());
-            return;
-        }
-
-        for variant in &parts[0] {
-            current.extend(variant);
-            combine(current, &parts[1..], result);
-            current.truncate(current.len() - variant.len());
-        }
-    }
-
-    let mut result = Vec::new();
-    combine(&mut Vec::new(), &moves, &mut result);
-    result
-}
-
-
-fn solve_p1(input: &str, num_robots: usize) -> usize {
-    let xs = parse(input);
-    let num_pad = get_numpad();
-    let dir_pad = get_dirpad();
-
-    let mut sum = 0;
-    for code in xs {
-        print!("Code: {}", code);
-        let code_chars = code.chars().collect::<Vec<_>>();
-        let mut dirs1 = get_all_variants(&code_chars, &num_pad);
-
-        for i in 0..num_robots {
-            println!(" Iteration {}", i);
-            let dirs2 = all_possible_moves(dirs1, &dir_pad);
-            //println!("Shortest step 2: {}", dirs2_min);
-
-            dirs1 = dirs2;
-        }
-
-        let dirs1_min = dirs1.iter().map(|xs| xs.len()).min().unwrap();
-        let part_code = code[0..=2].parse::<usize>().unwrap();
-        let prod = dirs1_min * part_code;
-        sum += prod;
-
-        // let dirs2 = get_directions(&dirs1, &dir_pad);
-        // let dirs3 = get_directions(&dirs2, &dir_pad);
-        // let dirs3 = String::from_iter(dirs3);
-        // println!("{}", dirs3);
-    }
-
-    sum
-}
-
-fn all_possible_moves(dirs1: Vec<Moves>, dir_pad: &Pad) -> Vec<Moves> {
-    let mut dirs2 = Vec::new();
-    for dir1 in &dirs1 {
-        let dirs2s = get_all_variants(dir1, dir_pad);
-        // let dirs2s1 = dirs2s[0].clone();
-        // dirs2.push(dirs2s1);
-        dirs2.extend(dirs2s);
- 
-        // let shortest_len = dirs2s.iter().map(|variant| variant.len()).min().unwrap();
-        // let dirs2s: Vec<_> = dirs2s.into_iter().filter(|variant| variant.len() == shortest_len).collect();
-        // // println!("Shortest variant: {}", shortest_len);
-        // // println!("{:?}", dirs2);
-        // dirs2.push(dirs2s[0].clone());
-    }
-
-    let dirs2_min = dirs2.iter().map(|xs| xs.len()).min().unwrap_or(0);
-    let dirs2: Vec<_> = dirs2.into_iter().filter(|variant| variant.len() <= dirs2_min).collect();
-    println!("Num variants: {}", dirs2.len());
-    let dirs2: HashSet<Moves> = dirs2.into_iter().collect();
-    let dirs2: Vec<Moves> = dirs2.into_iter().collect();
-    dirs2
-}
-
-fn get_all_variants(code_chars: &Vec<char>, num_pad: &HashMap<char, Vec2i>) -> Vec<Moves> {
-    let dirs1 = get_directions(&code_chars, num_pad);
-    // robot 1 contolling num pad
-    let dirs1 = all_variants(dirs1);
-    let dirs1: Vec<Moves> = dirs1.into_iter().map(|variant| variant.into_iter().rev().collect()).collect();
-    dirs1
-}
-
 type Pad = HashMap<char, Vec2i>;
-type Moves = Vec<char>;
+type Moves = VecDeque<char>;
 
-fn get_directions(code: &[char], pad: &Pad) -> Vec<Vec<Moves>> {
-    let current_key = 'A';
-    let mut directions = Vec::new();
-    let mut current_pos: Vec2i = pad[&current_key].clone();
-    for key in code {
-        let target_pos = &pad[key];
-        let all_moves = get_directions_to_target(&current_pos, target_pos, pad);
-        //println!("{:?}", all_moves);
-        directions.push(all_moves);
-        current_pos = target_pos.clone();
-    }
-    //println!("{:?}", &directions);
-
-    directions
-}
-
-fn get_directions_to_target(current_pos: &Vec2i, target_pos: &Vec2i, pad: &Pad) -> Vec<Moves> {
-    if current_pos == target_pos {
-        return vec![ vec!['A'] ];
+fn get_directions_to_target(target_start: &Vec2i, target_end: &Vec2i, pad: &Pad) -> Vec<Moves> {
+    if target_start == target_end {
+        let mut moves = Moves::new();
+        moves.push_back('A');
+        return vec![moves];
     }
 
     let invalid_pos = &pad[&'_'];
 
-    let dir = target_pos.clone() - current_pos.clone();
+    let dir = target_end.clone() - target_start.clone();
 
-    let mut dirs = vec![];
-
+    let mut dirs = VecDeque::new();
     if dir.x > 0 {
-        let next = current_pos + Vec2i::new(1, 0);
-        if next != *invalid_pos {
-            let mut all_dirs = get_directions_to_target(&next, target_pos, pad);
-            for dirs in &mut all_dirs {
-                dirs.push('>');
-            }
-            dirs.extend(all_dirs);
+        for _ in 0..dir.x.abs() {
+            dirs.push_back('>')
         }
     }
     if dir.x < 0 {
-        let next = current_pos + Vec2i::new(-1, 0);
-        if next != *invalid_pos {
-            let mut all_dirs = get_directions_to_target(&next, target_pos, pad);
-            for dirs in &mut all_dirs {
-                dirs.push('<');
-            }
-            dirs.extend(all_dirs);
+        for _ in 0..dir.x.abs() {
+            dirs.push_back('<')
         }
     }
-
     if dir.y > 0 {
-        let next = current_pos + Vec2i::new(0, 1);
-        if next != *invalid_pos {
-            let mut all_dirs = get_directions_to_target(&next, target_pos, pad);
-            for dirs in &mut all_dirs {
-                dirs.push('v');
-            }
-            dirs.extend(all_dirs);
+        for _ in 0..dir.y.abs() {
+            dirs.push_back('v')
         }
     }
     if dir.y < 0 {
-        let next = current_pos + Vec2i::new(0, -1);
-        if next != *invalid_pos {
-            let mut all_dirs = get_directions_to_target(&next, target_pos, pad);
-            for dirs in &mut all_dirs {
-                dirs.push('^');
-            }
-            dirs.extend(all_dirs);
+        for _ in 0..dir.y.abs() {
+            dirs.push_back('^')
         }
     }
 
-    dirs
+    let n = dirs.len();
+    let valid_moves: Vec<VecDeque<_>> = dirs
+        .into_iter()
+        .permutations(n)
+        .unique()
+        .filter_map(|moves| {
+            let mut next = target_start.clone();
+            for move_char in &moves {
+                match move_char {
+                    '>' => next.x += 1,
+                    '<' => next.x -= 1,
+                    'v' => next.y += 1,
+                    '^' => next.y -= 1,
+                    _ => unreachable!(),
+                }
+                if next == *invalid_pos || !pad.values().contains(&next) {
+                    return None;
+                }
+            }
+            let mut current: Moves = moves.into_iter().collect();
+            current.push_back('A');
+            Some(current)
+        })
+        .collect();
+    valid_moves
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+fn solve(input: &str, num_robots: usize) -> usize {
+    let xs = parse(input);
+    let num_pad = get_numpad();
+    let dir_pad = get_dirpad();
 
-//     #[test]
-//     fn p1_test1() {
-//     }
+    let pads = vec![num_pad, dir_pad];
 
-//     #[test]
-//     fn p2_test1() {
-//     }
-// }
+    let shortest_move_len_cache = Rc::new(RefCell::new(HashMap::new()));
 
-const INPUT_EX1: &str = r#"029A
+    let mut sum = 0;
+    for code in xs {
+        let code_chars = code.chars().collect::<VecDeque<_>>();
+
+        let min_len = get_shortest_move_len(
+            &code_chars,
+            num_robots,
+            &pads,
+            1,
+            0,
+            shortest_move_len_cache.clone(),
+        );
+
+        let part_code = code[0..=2].parse::<usize>().unwrap();
+        let prod = min_len * part_code;
+        sum += prod;
+    }
+    sum
+}
+
+fn get_shortest_move_len(
+    code_chars: &VecDeque<char>,
+    num_robots: usize,
+    pads: &[Pad],
+    source_pad: usize,
+    target_pad: usize,
+    shortest_move_len_cache: Rc<RefCell<HashMap<(VecDeque<char>, usize, usize, usize), usize>>>,
+) -> usize {
+    let key = (code_chars.clone(), num_robots, source_pad, target_pad);
+    if let Some(&len) = shortest_move_len_cache.borrow().get(&key) {
+        return len;
+    }
+
+    let mut code_chars2 = vec!['A'];
+    code_chars2.extend(code_chars);
+
+    let mut total_length = 0;
+    for w in code_chars2.windows(2) {
+        let target_start_char = w[0];
+        let target_end_char = w[1];
+        let target_start = &pads[target_pad][&target_start_char];
+        let target_end = &pads[target_pad][&target_end_char];
+
+        let all_paths = get_directions_to_target(target_start, target_end, &pads[target_pad]);
+        if num_robots == 0 {
+            total_length += all_paths.iter().map(|path| path.len()).min().unwrap();
+        } else {
+            total_length += all_paths
+                .iter()
+                .map(|path| {
+                    get_shortest_move_len(
+                        path,
+                        num_robots - 1,
+                        pads,
+                        source_pad,
+                        source_pad,
+                        shortest_move_len_cache.clone(),
+                    )
+                })
+                .min()
+                .unwrap();
+        }
+    }
+
+    shortest_move_len_cache
+        .borrow_mut()
+        .insert(key, total_length);
+
+    total_length
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT_EX1: &str = r#"029A
 980A
 179A
 456A
 379A"#;
 
+    #[test]
+    fn p1_test1() {
+        assert_eq!(solve(INPUT_EX1, 2), 126384);
+    }
+}
+
 fn main() {
-    assert_eq!(solve_p1(INPUT_EX1,2 ), 126384);
-    // assert_eq!(solve_p2(INPUT_EX1,), );
-    println!("All tests passed!");
     let args: Vec<String> = env::args().collect();
     let input_file = if args.len() > 1 {
         &args[1]
@@ -230,6 +214,6 @@ fn main() {
         std::process::exit(1);
     });
 
-    println!("Part1: {}", solve_p1(&input,2));
-    println!("Part2: {}", solve_p1(&input,25));
+    println!("Part1: {}", solve(&input, 2));
+    println!("Part2: {}", solve(&input, 25));
 }
